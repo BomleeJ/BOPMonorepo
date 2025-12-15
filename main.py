19cd128831e9d1c0a13f7c83be5b8e803805baf6
OmarJ <omarkjingo@gmail.com>
2025-12-14 21:26:29 -0500

    CSV INput

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import httpx
 import pprint
+from internal_data.ArbGroup import ArbGroup
+from internal_data.MarketData import MarketData
+import csv
 
-import httpx
+def read_csv(csv_name: str) -> list[ArbGroup]:
+    arbGroups = []
+    with open(csv_name, newline='') as file:
+        reader = csv.reader(file, delimiter=",")
+        next(reader)
+        it = iter(reader)
+        for row1, row2 in zip(it, it):
+            arb_id = row1[0]
+            eventURL1 = row1[1]
+            eventURL2 = row2[1]
 
-# url = "https://gamma-api.polymarket.com/events/slug/will-draftkings-launch-a-prediction-market-in-2025"
+            groupItemTitles1 = { row1[i] for i in range(2, len(row1)) if row1[i] }
+            groupItemTitles2 = { row2[i] for i in range(2, len(row2)) if row2[i] }
+
+            if not groupItemTitles1:
+                groupItemTitles1 = None
+
+            if not groupItemTitles2:
+                groupItemTitles2 = None
+            a = ArbGroup(eventURL1, eventURL2, groupItemTitles1, groupItemTitles2)
+            arbGroups.append(a)
+            print(a)
+    
+    return arbGroups
+
+if __name__ == '__main__':
+    read_csv("din.csv")
 
 
-url = "https://gamma-api.polymarket.com/events/slug/democratic-presidential-nominee-2028"
-
-res = httpx.get(url)
-
-print(res.json())
 """
 
 from py_clob_client.client import ClobClient
